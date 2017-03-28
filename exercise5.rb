f749f13b1cd7c8a2c165166e611fc7cbc3895417
Azbagheri <azadeh.bagheri7@gmail.com>
2017-03-27 23:57:41 -0400

    The initial energy level is changed in exercise6

--- exercise5.rb
+++ exercise5.rb
@@ -1,12 +1,13 @@
 
-puts "How many pizzas are you ordering?"
-quantity = gets.chomp.to_i
+  puts "How many pizzas are you ordering?"
+  quantity = gets.chomp.to_i
 
-topping =[]
-quantity.times do |num|
-  puts" what topping do you need for pizza number #{num + 1}?"
-  topping[num] = gets.chomp
-end
-topping.each do |item|
- puts "You have ordered a pizze with #{item} toppings."
-end
+  topping =[]
+  quantity.times do |num|
+     puts" what topping do you need for pizza number #{num + 1}?"
+     topping[num] = gets.chomp
+  end
+
+  topping.each do |item|
+     puts "You have ordered a pizze with #{item} toppings."
+  end
